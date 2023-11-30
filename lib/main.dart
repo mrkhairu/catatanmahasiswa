8f77805e7d131806f56b207b9722ba2e02093a4e
mrkhairu <dafkhairulammar@gmail.com>
2023-11-30 08:12:28 +0700

    changes

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:catatanmahasiswa/firebase_options.dart';
+import 'package:catatanmahasiswa/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+} 
+class MyApp extends StatefulWidget {
+  const MyApp ({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
